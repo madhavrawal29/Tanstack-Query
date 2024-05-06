3f4a8e88f9351dbc1e2d63884a41a21b37e49b83
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-05-06 10:28:17 +0530

    Cache, staleTime and gcTime in react query

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -9,6 +9,8 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events"],
     queryFn: fetchEvents,
+    staleTime: 5000, // will wait for 5 secs before sending another request
+    gcTime: 1000, // garbage collection time for cache
   });
 
   let content;
