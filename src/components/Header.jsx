39965b03d588d81ef15c2a2c2cc12315e691ac36
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-05-09 14:18:02 +0530

    React Query and React Router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header({ children }) {
+  const fetching = useIsFetching();
+  // when the data in fetched then fetching value is 0 and
+  // if the data is not fetched or in the fecthing condition fetching value will be greater than 0
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">{fetching > 0 && <progress />}</div>
+
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
